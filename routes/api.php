cf0cfa8d8b5d2444dfe64efa1c7657e1b9866a5b
Mourad <yas20072003@gmail.com>
2025-03-15 08:47:17 +0000

    move auth routes to auth.php

--- routes/api.php
+++ routes/api.php
@@ -2,29 +2,29 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\VerifyEmailController;
-use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+// use App\Http\Controllers\Auth\RegisteredUserController;
+// use App\Http\Controllers\Auth\AuthenticatedSessionController;
+// use App\Http\Controllers\Auth\PasswordResetLinkController;
+// use App\Http\Controllers\Auth\NewPasswordController;
+// use App\Http\Controllers\Auth\VerifyEmailController;
+// use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 
-Route::post('/register', [RegisteredUserController::class, 'store'])->middleware('guest');
+// Route::post('/register', [RegisteredUserController::class, 'store'])->middleware('guest');
 
-Route::post('/login', [AuthenticatedSessionController::class, 'store'])->middleware('guest');
+// Route::post('/login', [AuthenticatedSessionController::class, 'store'])->middleware('guest');
 
-Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])->middleware('guest');
+// Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])->middleware('guest');
 
-Route::post('/reset-password', [NewPasswordController::class, 'store'])->middleware('guest');
+// Route::post('/reset-password', [NewPasswordController::class, 'store'])->middleware('guest');
 
-Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
-    ->middleware(['auth:sanctum', 'signed', 'throttle:6,1']);
+// Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
+//     ->middleware(['auth:sanctum', 'signed', 'throttle:6,1']);
 
-Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-    ->middleware(['auth:sanctum', 'throttle:6,1']);
+// Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+//     ->middleware(['auth:sanctum', 'throttle:6,1']);
 
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-    ->middleware('auth:sanctum');
+// Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+//     ->middleware('auth:sanctum');
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
